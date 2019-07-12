d71bce6425bcf0de206596d49863221e8c49d741
Louis Coffman <bacchinal@gmail.com>
2019-07-12 17:40:35 -0500

    added user and entry classes

--- main.py
+++ main.py
@@ -7,9 +7,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:admin@loca
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
+@app.route('/add-blog', methods=['GET','POST'])
+def add():
     return render_template('add-blog.html')
+    return redirect('/')
+
+@app.route('/main-page', methods=['POST'])
+def index():
+    return render_template('main-page')
+
+class Entry(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, owner ):
+        self.title = title
+        self.completed = False
+        self.owner = owner
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    tasks = db.relationship('Entry', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
